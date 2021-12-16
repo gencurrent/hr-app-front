642b66a51b5339ca7692e57e41326a886c082351
Artem Bulatov <gencurrent@gmail.com>
2021-12-16 03:49:43 +0300

    🖥 Improved appearance of Submission answers block

--- src/component/SubmissionListItemAnswer/SubmissionListItemAnswer.js
+++ src/component/SubmissionListItemAnswer/SubmissionListItemAnswer.js
@@ -2,11 +2,20 @@ import { React } from 'react';
 import PropTypes from 'prop-types';
 
 import {
+  makeStyles,
   Link,
-  Typography
+  Typography,
+  Box,
+  Button
 } from '@material-ui/core';
 
+const useStyles = makeStyles(theme => ({
+  answerContainer: {
+    margin: theme.spacing(1, 0, 1, 3)
+  }
+}));
 function SubmissionListItemAnswer(props) {
+  const classes = useStyles();
   const {answer, vacancy} = props;
   const answerFull = {...JSON.parse(vacancy.fields).find(el => el.q === answer.q), ...answer};
   const fileUrlBase = process.env.REACT_APP_CLOUD_STORAGE_URL_BASE;
@@ -14,22 +23,25 @@ function SubmissionListItemAnswer(props) {
     <>
       <div className='answer-block'>
         
-        <Typography>{answer.q}</Typography>
-        {
-        answer.a === null ? <Typography component='p'>Empty</Typography>
-        :
-        answerFull.t === 'file' && <>
-         <Link target="_blank" download href={`${fileUrlBase}/${answer.a}`}>Download</Link>
-        </>}
-        {answerFull.t === 'line' && <>
-          <p>{answer.a}</p>
-        </>}
-        {answerFull.t === 'text' && <>
-          <p>{answer.a}</p>
-        </>}
-        {answerFull.t === 'number' && <>
-          <p>{answer.a}</p>
-        </>}
+        <Typography variant='subtitle1' style={{fontWeight: 600}}>{answer.q}</Typography>
+        <Box className={classes.answerContainer}>
+          {
+            answer.a === null ? <Typography component='p'>Empty</Typography>
+            :
+            answerFull.t === 'file' && <>
+            <Link target="_blank" download href={`${fileUrlBase}/${answer.a}`}><Button variant='outlined' color='primary'>Download</Button></Link>
+            </>}
+            {answerFull.t === 'line' && <>
+              <p>{answer.a}</p>
+            </>}
+            {answerFull.t === 'text' && <>
+              <p>{answer.a}</p>
+            </>}
+            {answerFull.t === 'number' && <>
+              <p>{answer.a}</p>
+            </>
+          }
+        </Box>
         {/* {answerFull.t === 'date' && <>
           <p>{answer.a}</p>
         </>} */}

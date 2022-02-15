659f7377173b263a7ca9691c66fce690772be251
Artem Bulatov <gencurrent@gmail.com>
2022-02-15 21:42:12 +0300

    ⚙️ Updated routes according to the submissionList method

--- src/utils/apollo/queries/index.js
+++ src/utils/apollo/queries/index.js
@@ -5,70 +5,86 @@
 import { gql } from '@apollo/client';
 
 const QUERIES = {
-    TOKEN_AUTH: gql`
-    query TokenAuth($username: String!, $password: String!){
-        tokenAuth(username: $username, password: $password){
-            token
-            refreshToken
-        }
-    }`,
-    REFRESH: gql`query refresh($refreshToken: String!){refreshToken(refreshToken: $refreshToken){
-        token
-        payload
-    }}`,
-    VACANCY_LIST:  gql`query vacancyList{
-       vacancyList{
-           id
-           company
-           position
-           text
-           submissionCountTotal
-       } 
-    }`,
-    VACANCY: gql`query vacancy($id: ID!, $forSubmission: Boolean = null){
-        vacancy(id: $id, forSubmission: $forSubmission){
-            uuid
-            submissionUUID
-            position
-            company
-            text
-            fields
-            ts
-        }
-    }`,
-    VACANCY_WITH_SUBMISSION_LIST: gql`query vacancy($id: ID!){
-        vacancy(id: $id){
-            uuid
-            position
-            company
-            text
-            fields
-            submissionList {
-                uuid
-                fullname
-                email
-                phone
-                resume
-                answers
-                decision
-                comment
-                ts
-            }
-        }
-    }`,
-    USER_MAIN_STATS:  gql`query userMainStats{
-        userMainStats{
-            submissionCountTotal
-            submissionCountNew
-            vacancyStatsList{
-                id
-                position
-                company
-                submissionCountTotal
-                submissionCountNew
-            }
-        }
-     }`,
+  TOKEN_AUTH: gql`
+  query TokenAuth($username: String!, $password: String!){
+      tokenAuth(username: $username, password: $password){
+          token
+          refreshToken
+      }
+  }`,
+  REFRESH: gql`query refresh($refreshToken: String!){refreshToken(refreshToken: $refreshToken){
+      token
+      payload
+  }}`,
+  VACANCY_LIST:  gql`query vacancyList{
+     vacancyList{
+         id
+         company
+         position
+         text
+         submissionCountTotal
+     } 
+  }`,
+  VACANCY: gql`query vacancy($id: ID!, $forSubmission: Boolean = null){
+      vacancy(id: $id, forSubmission: $forSubmission){
+          uuid
+          submissionUUID
+          position
+          company
+          text
+          fields
+          ts
+      }
+  }`,
+  VACANCY_WITH_SUBMISSION_LIST: gql`query vacancy($id: ID!){
+    vacancy(id: $id){
+      uuid
+      position
+      company
+      text
+      fields
+      submissionList {
+        uuid
+        fullname
+        email
+        phone
+        resume
+        answers
+        decision
+        comment
+        ts
+      }
+    }
+  }`,
+  SUBMISSION_LIST: gql`query submissionList($vacancyId: ID){
+    submissionList(vacancyId: $vacancyId){
+      uuid
+      fullname
+      email
+      phone
+      resume
+      answers
+      decision
+      comment
+      ts
+      vacancy {
+          
+      }
+    }
+  }`,
+  USER_MAIN_STATS:  gql`query userMainStats{
+      userMainStats{
+          submissionCountTotal
+          submissionCountNew
+          vacancyStatsList{
+              id
+              position
+              company
+              submissionCountTotal
+              submissionCountNew
+          }
+      }
+  }`,
     
 };
 

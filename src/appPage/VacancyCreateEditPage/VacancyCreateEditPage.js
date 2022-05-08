9bb3fe6789f6c926dc8cd410e4f46e9b554a8424
Artem Bulatov <gencurrent@gmail.com>
2022-05-09 00:14:02 +0200

    ✨ Redirection to the Vacancy page after creating it

--- src/appPage/VacancyCreateEditPage/VacancyCreateEditPage.js
+++ src/appPage/VacancyCreateEditPage/VacancyCreateEditPage.js
@@ -1,104 +1,152 @@
-import PropTypes from 'prop-types';
-import { React, useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { useHistory } from 'react-router-dom';
-import { 
-    TextField,
-    FormControl,
-    FormHelperText, 
-    Typography,
-    Button,
-    Grid
-} from '@mui/material';
+import PropTypes from "prop-types";
+import { React, useState } from "react";
+import { useMutation } from "@apollo/client";
+import { useHistory } from "react-router-dom";
+import {
+  TextField,
+  FormControl,
+  FormHelperText,
+  Typography,
+  Button,
+  Grid,
+} from "@mui/material";
 
-import { VacancyCUFieldList } from 'component';
+import { VacancyCUFieldList } from "component";
 import {
   FIELD_VACANCY_HELPER_TEXT_DEFAULT,
-  FIELD_VACANCY_HELPER_TEXT_DEFAULT_LENGTH_ERROR
-} from './constants';
-import { MUTATIONS } from 'utils/apollo';
-
-
+  FIELD_VACANCY_HELPER_TEXT_DEFAULT_LENGTH_ERROR,
+} from "./constants";
+import { MUTATIONS } from "utils/apollo";
 
 const TEXT_FIELD_ROWS_NUMBER_DEFAULT = 3;
 
-const VacancyCreateEditPage = (props) => {    
-    let history = useHistory();
-    let [position, setPosition] = useState('');
-    let [company, setCompany] = useState('');
-    let [text, setText] = useState('');
-    let [fields, setFields] = useState([]);
+const VacancyCreateEditPage = (props) => {
+  let history = useHistory();
+  let [position, setPosition] = useState("");
+  let [company, setCompany] = useState("");
+  let [text, setText] = useState("");
+  let [fields, setFields] = useState([]);
 
-    let [rowsNumber, setRowsNumber] = useState(5);
-    
-    const [positionFieldHelperProps, setPositionFieldHelperProps] = useState({'error': false, 'text': FIELD_VACANCY_HELPER_TEXT_DEFAULT});
+  let [rowsNumber, setRowsNumber] = useState(5);
 
-    const updateFormPosition = e => {
-        let value = e.currentTarget.value;
-        if (value.length < 10){
-            setPositionFieldHelperProps({'error': true, 'text': FIELD_VACANCY_HELPER_TEXT_DEFAULT_LENGTH_ERROR});
-        }
-        else {
-            setPositionFieldHelperProps({'error': false, 'text': FIELD_VACANCY_HELPER_TEXT_DEFAULT});
-        }
-        setPosition(value);
-    }
+  const [positionFieldHelperProps, setPositionFieldHelperProps] = useState({
+    error: false,
+    text: FIELD_VACANCY_HELPER_TEXT_DEFAULT,
+  });
 
-    const updateFormText = e => {
-      let strToCheck = RegExp('\n', 'g')
-      let matchesReg = e.currentTarget.value.matchAll(strToCheck)
-      let newRowsNumber = Array.from(matchesReg).length + 1;
-      setRowsNumber(
-          newRowsNumber > TEXT_FIELD_ROWS_NUMBER_DEFAULT ? newRowsNumber : TEXT_FIELD_ROWS_NUMBER_DEFAULT
-      );
-      setText(e.currentTarget.value);
-    }
-
-    
-    let [createVacancy, {data, loading, error }] = useMutation(MUTATIONS.CREATE_VACANCY);
-
-    const save = (e) => {
-      let data = {
-        position: position,
-        company: company,
-        text: text,
-        fields: fields,
-      }
-      createVacancy({
-        variables: {
-          company: company,
-          position: position,
-          text: text,
-          fields: JSON.stringify(fields)
-        }
-      }).then(data => {
-          // Success only
-          history.push('/vacancy');
+  const updateFormPosition = (e) => {
+    let value = e.currentTarget.value;
+    if (value.length < 10) {
+      setPositionFieldHelperProps({
+        error: true,
+        text: FIELD_VACANCY_HELPER_TEXT_DEFAULT_LENGTH_ERROR,
+      });
+    } else {
+      setPositionFieldHelperProps({
+        error: false,
+        text: FIELD_VACANCY_HELPER_TEXT_DEFAULT,
       });
     }
-    return (
-      <Grid container direction='column' spacing={2}>
-        <Grid item sm={12}>
-          <Typography variant='h4' component='h3'>New vacancy</Typography>
-          <FormControl className='' noValidate autoComplete='off' fullWidth={true} >
-            <TextField label='Position' error={positionFieldHelperProps['error']} autoComplete='position' id='position' value={position} onChange={updateFormPosition} required />
-            <FormHelperText error={positionFieldHelperProps['error']}>{positionFieldHelperProps['text']}</FormHelperText>
-            <TextField label='Company' autoComplete='company' id='company' value={company} onChange={(e) => setCompany(e.currentTarget.value)} required />
-            <FormHelperText>An organization to show to a candidate</FormHelperText>
-            <TextField multiline rows={rowsNumber} label='Description' id='text' value={text} onChange={updateFormText} required />
-            <FormHelperText>The description of vacancy a candidate will see</FormHelperText>
-            <VacancyCUFieldList fields={fields} setFields={setFields} />
-          </FormControl>
-          </Grid>
-          <Grid item xs={12}>
-          <Button variant='contained' color='primary' onClick={save}>Save</Button>
-          </Grid>
-        </Grid>
-    )
+    setPosition(value);
+  };
+
+  const updateFormText = (e) => {
+    let strToCheck = RegExp("\n", "g");
+    let matchesReg = e.currentTarget.value.matchAll(strToCheck);
+    let newRowsNumber = Array.from(matchesReg).length + 1;
+    setRowsNumber(
+      newRowsNumber > TEXT_FIELD_ROWS_NUMBER_DEFAULT
+        ? newRowsNumber
+        : TEXT_FIELD_ROWS_NUMBER_DEFAULT
+    );
+    setText(e.currentTarget.value);
+  };
+
+  let [createVacancy, { data, loading, error }] = useMutation(
+    MUTATIONS.CREATE_VACANCY
+  );
+
+  const save = (e) => {
+    let data = {
+      position: position,
+      company: company,
+      text: text,
+      fields: fields,
+    };
+    createVacancy({
+      variables: {
+        company: company,
+        position: position,
+        text: text,
+        fields: JSON.stringify(fields),
+      },
+    }).then(({ data }) => {
+      // Success only
+      console.log("data = ", data);
+      history.push(`/vacancy/${data.createVacancy.id}`);
+    });
+  };
+  return (
+    <Grid container direction="column" spacing={2}>
+      <Grid item sm={12}>
+        <Typography variant="h4" component="h3">
+          New vacancy
+        </Typography>
+        <FormControl
+          className=""
+          noValidate
+          autoComplete="off"
+          fullWidth={true}
+        >
+          <TextField
+            label="Position"
+            error={positionFieldHelperProps["error"]}
+            autoComplete="position"
+            id="position"
+            value={position}
+            onChange={updateFormPosition}
+            required
+          />
+          <FormHelperText error={positionFieldHelperProps["error"]}>
+            {positionFieldHelperProps["text"]}
+          </FormHelperText>
+          <TextField
+            label="Company"
+            autoComplete="company"
+            id="company"
+            value={company}
+            onChange={(e) => setCompany(e.currentTarget.value)}
+            required
+          />
+          <FormHelperText>
+            An organization to show to a candidate
+          </FormHelperText>
+          <TextField
+            multiline
+            rows={rowsNumber}
+            label="Description"
+            id="text"
+            value={text}
+            onChange={updateFormText}
+            required
+          />
+          <FormHelperText>
+            The description of vacancy a candidate will see
+          </FormHelperText>
+          <VacancyCUFieldList fields={fields} setFields={setFields} />
+        </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" color="primary" onClick={save}>
+          Save
+        </Button>
+      </Grid>
+    </Grid>
+  );
 };
 
 VacancyCreateEditPage.propTypes = {
-    vacancyId: PropTypes.number
-}
+  vacancyId: PropTypes.number,
+};
 
 export default VacancyCreateEditPage;
--- src/appPage/VacancyPage/VacancyPage.js
+++ src/appPage/VacancyPage/VacancyPage.js
@@ -1,106 +1,118 @@
-import { React } from 'react';
+/**
+ * The created Vacancy page
+ * Funcitons:
+ * + shows all the main info about the Vacancy
+ * + contains all the Vacancy actions
+ */
+import { React } from "react";
 import { makeStyles } from "@mui/styles";
-import {
-  Typography,
-  Grid,
-  Card
-} from '@mui/material';
+import { Typography, Grid, Card } from "@mui/material";
 
-import { datetimeToString } from 'utils/date';
-import { QUERIES } from 'utils/apollo';
-import { useParams } from 'react-router';
-import { useQuery } from '@apollo/client';
+import { datetimeToString } from "utils/date";
+import { QUERIES } from "utils/apollo";
+import { useParams } from "react-router";
+import { useQuery } from "@apollo/client";
 
 const useStyles = makeStyles((theme) => ({
   questionItem: {
-    margin: theme.spacing(.1, 0),
-    padding: theme.spacing(.5)
+    margin: theme.spacing(0.1, 0),
+    padding: theme.spacing(0.5),
   },
   questionCard: {
-    padding: theme.spacing(1)
+    padding: theme.spacing(1),
   },
   textBold: {
-    fontWeight: 700
-  }
+    fontWeight: 700,
+  },
 }));
 
 /**
  * The Vacancy Page hook
  * @param {} props
- * @returns 
+ * @returns
  */
 function VacancyPage(props) {
   const params = useParams();
-  const {id: vacancyId } = params;
+  const { id: vacancyId } = params;
   const classes = useStyles();
-  const { loading, error, data } = useQuery(
-    QUERIES.VACANCY,
-    {
-      variables: {
-        id: vacancyId
-      }
-    }
+  const { loading, error, data } = useQuery(QUERIES.VACANCY, {
+    variables: {
+      id: vacancyId,
+    },
+  });
+  return (
+    <>
+      {loading && (
+        <>
+          <Typography>Loading</Typography>
+        </>
+      )}
+      {error && (
+        <>
+          <Typography>Error</Typography>
+        </>
+      )}
+      {data && (
+        <>
+          <Typography variant="h4" component="h1" gutterBottom>
+            {data.vacancy.position}
+          </Typography>
+          <Card variant="outlined">
+            <Grid container>
+              <Grid item xs={12}>
+                <Typography component="span">Vacancy: </Typography>
+                <Typography className={classes.textBold} component="span">
+                  {data.vacancy.position}
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography component="span">Company: </Typography>
+                <Typography className={classes.textBold} component="span">
+                  {data.vacancy.company}
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography component="span">Created: </Typography>
+                <Typography className={classes.textBold} component="span">
+                  {datetimeToString(new Date(data.vacancy.ts))}
+                </Typography>
+              </Grid>
+            </Grid>
+
+            <Typography>Fields:</Typography>
+            <Grid container>
+              {JSON.parse(data.vacancy.fields).map((field, idx) => {
+                return (
+                  <Grid item xs={12} className={classes.questionItem}>
+                    <Card variant="outlined" className={classes.questionCard}>
+                      <Typography className={classes.textBold} component="span">
+                        {idx + 1}.{" "}
+                      </Typography>
+                      <Typography className={classes.textBold} component="span">
+                        {field.r ? "[Required] " : ""}
+                      </Typography>
+                      <Typography className={classes.textBold} component="span">
+                        {field.q}
+                      </Typography>
+                      <div>
+                        <Typography component="span">Type:</Typography>
+                        <Typography
+                          component="span"
+                          className={classes.textBold}
+                        >
+                          {field.t.replace(/./, (c) => c.toUpperCase())}
+                        </Typography>
+                      </div>
+                    </Card>
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </Card>
+        </>
+      )}
+    </>
   );
-  return (<>
-    {loading && <>
-      <Typography>Loading</Typography>
-    </>}
-    {error && <>
-      <Typography>Error</Typography>
-    </>}
-    {data &&
-      <>
-      <Typography
-        variant='h4' component='h1'
-        gutterBottom
-      >{data.vacancy.position}</Typography>
-      <Card variant='outlined'>
-        <Grid container>
-          <Grid item xs={12}>
-            <Typography component='span'>Vacancy: </Typography>
-            <Typography className={classes.textBold} component='span'>{data.vacancy.position}</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography component='span'>Company: </Typography>
-            <Typography className={classes.textBold} component='span'>{data.vacancy.company}</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography component='span'>Created: </Typography>
-            <Typography className={classes.textBold} component='span'>{datetimeToString(new Date(data.vacancy.ts))}</Typography>
-          </Grid>
-        </Grid> 
-        
-          
-          <Typography>Fields:</Typography>
-          <Grid container>
-            {JSON.parse(data.vacancy.fields).map((field, idx) => {
-              return(
-                <Grid
-                  item xs={12}
-                  className={classes.questionItem}
-                >
-                  <Card variant='outlined' className={classes.questionCard}>
-                    <Typography className={classes.textBold} component='span'>{idx + 1}. </Typography>
-                    <Typography className={classes.textBold} component='span' >{field.r ? '[Required] ' : ''}</Typography>
-                    <Typography className={classes.textBold} component='span'>{field.q}</Typography>
-                    <div>
-                      <Typography component="span">Type:</Typography><Typography component="span" className={classes.textBold}>{field.t.replace(/./, c => c.toUpperCase())}</Typography>
-                      
-                    </div>
-                    
-                  </Card>
-                </Grid>
-              )
-
-            })}
-          </Grid>
-
-        </Card>
-      </>
-    }
-  </>);
-};
-
-
+}
 
 export default VacancyPage;

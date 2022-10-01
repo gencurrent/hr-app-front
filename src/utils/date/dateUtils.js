e0839c6ec94fa8312481b53a4626ecb1a14bfd51
Artem Bulatov <gencurrent@gmail.com>
2022-10-01 15:54:52 +0200

    ✨ Local timezone support

--- src/utils/date/dateUtils.js
+++ src/utils/date/dateUtils.js
@@ -1,11 +1,13 @@
 /**
  * The utils to work with date formatting
  */
+import moment from "moment-timezone";
 
-function datetimeToString(datetime) {
-    const reg = /^(?<date>\d{4}-\d{2}-\d{2})T(?<hour>\d{2}):(?<minute>\d{2}).*$/gm ;
-    const matches = datetime.toISOString().matchAll(reg).next().value.groups;
-    return `${matches["date"]} ${matches["hour"]}:${matches["minute"]}`;
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+function datetimeToString(datetimeString) {
+  const timestampFormatted = moment.tz(datetimeString, timezone);
+  return timestampFormatted.format("YYYY-MM-DD HH:mm");
 }
 
-export {datetimeToString};
+export { datetimeToString };
